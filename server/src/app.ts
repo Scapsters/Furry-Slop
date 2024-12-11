4c243a6864697b580002678f926e71136af5f695
Scapsters <scotty.happy@gmail.com>
2024-12-11 14:12:17 -0500

    refactor for clarity

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import getRandomImage, { makePath } from './images.ts';
 
-const app = express();
-const port = 3000;
+const DEV = true;
+const port = DEV ? 5000 : 80;
 
-app.use('/.well-known/acme-challenge', express.static(makePath('/.well-known/acme-challenge')));
+const defaultRequest = (req: Request, res: Response): void => {
+    console.log("query", req.query);
+    console.log("params", req.params);
+    console.log("header", req.headers);
+    console.log("body", req.body);
 
-app.get('/', (req, res) => {
-    console.log("query", req.query)
-    console.log("params", req.params)
-    console.log("header", req.headers)
-    console.log("body", req.body)
-
-
-    const imagePath = getRandomImage()
+    const imagePath = getRandomImage();
     res.sendFile(imagePath);
-})
+}
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on port ${port}`);
-})
+express()
+    .use('/.well-known/acme-challenge', express.static(makePath('/.well-known/acme-challenge')))
+    .get('/', defaultRequest)
+    .listen(port, '0.0.0.0', () => {console.log(`Server is running on port ${port}`)})
 

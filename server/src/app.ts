27907e0fbed62d500bf4222a2a46c6377b4e039c
Scapsters <scotty.happy@gmail.com>
2024-12-11 23:07:45 -0500

    server interacts with db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import DB_RESTART from './db.ts';
 import path from 'path';
 
 import getRandomImage, { makePath } from './images.ts';
@@ -8,24 +9,20 @@ import AbsolutePathToRepositoryRoot from './AbsolutePathToRepositoryRoot.ts';
 const DEV = true;
 const port = DEV ? 5000 : 80;
 
+// Set to true to RESET THE DATABASE. TURN IT OFF AFTER
+const RESET_DATABASE = false;
+
 const buildPath = path.join(AbsolutePathToRepositoryRoot, 'client', 'build');
-const indexPath = path.join(buildPath, 'index.html');
-
-const defaultRequest = (req: Request, res: Response): void => {
-    console.log("query", req.query);
-    console.log("params", req.params);
-    console.log("header", req.headers);
-    console.log("body", req.body);
-
-    res.sendFile(indexPath);
-}
-
 const RandomImage = (_, res: Response): void => res.sendFile(getRandomImage())
 
 express()
     .use('/.well-known/acme-challenge', express.static(makePath('/.well-known/acme-challenge')))
     .use(express.static(buildPath))
-    .get('/', express.static(path.join(AbsolutePathToRepositoryRoot, 'client', 'build')))
+    .get('/', express.static(buildPath))
     .get('/RandomImage', RandomImage)
     .listen(port, '0.0.0.0', () => {console.log(`Server is running on port ${port}`)})
 
+if (RESET_DATABASE) {
+    console.log("Resetting the database")
+    DB_RESTART()
+}

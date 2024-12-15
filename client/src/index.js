cc5099bed0aa947c2737d814c8354b2b9b19ef0c
Scapsters <scotty.happy@gmail.com>
2024-12-14 21:30:28 -0500

    remove css to prepare for dynamic css

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import reportWebVitals from './useless/reportWebVitals.js';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

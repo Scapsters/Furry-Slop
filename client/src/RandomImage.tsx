ca859573700c5c2ea5ee34efc935c693662bc278
Scapsters <scotty.happy@gmail.com>
2024-12-15 02:15:03 -0500

    fix some critical errors including
    call to localhost on client
    missing api routing

--- client/src/RandomImage.tsx
+++ client/src/RandomImage.tsx
@@ -4,11 +4,11 @@ import { createEmptyTweetData } from "./TweetViewer.tsx";
 import { useNavigate } from 'react-router-dom';
 
 export const RandomImage = () => {
-    const [tweetData, isLoading] = useAsync('RandomTweetData', createEmptyTweetData())
+    const [tweetData, isLoading] = useAsync('Api/RandomTweetData', createEmptyTweetData())
     const navigate = useNavigate()
 
     useEffect(() => {
-        if(!isLoading) navigate(`Api/Tweets/${tweetData.status_id}`, { replace: true })
+        if(!isLoading) navigate(`Tweets/${tweetData.status_id}`, { replace: true })
     }, [navigate, tweetData.status_id, isLoading])
     
     return null
--- client/src/UseAsync.tsx
+++ client/src/UseAsync.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const awaitServer = async <T,>(serverPath: string): Promise<T> => {
-    const response = await fetch(`http://localhost:5000/${serverPath}`)
+    const response = await fetch(`https://furryslop.com/${serverPath}`)
     if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
     return await response.json()
 }

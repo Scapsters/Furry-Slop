eb5e8c5fe750931a74c3defa9f87ac2fddfa503a
Scapsters <scotty.happy@gmail.com>
2025-02-13 10:45:12 -0500

    factor out post from home

--- client/src/App/Home.tsx
+++ client/src/App/Home.tsx
@@ -5,6 +5,7 @@ import "./Home.css";
 import useAsync from "../UseAsync.tsx";
 import { useNavigate, useParams } from "react-router-dom";
 import { emptyTweetData } from "../TweetData.tsx";
+import { Post } from "./Post.tsx";
 
 export const postContext = React.createContext<TweetData>(emptyTweetData);
 export const refreshContext = React.createContext<() => void>(() => {});
@@ -18,55 +19,12 @@ export const Home = () => {
 		navigate(`/`);
 	}, [navigate]);
 
-	const mediaUrls = tweetData.media_urls?.split(",");
-	const mediaTypes = tweetData.media_details?.map((media) => media.type);
-
-	console.log(mediaUrls);
-	console.log(mediaTypes);
-
-	let images;
-	if (mediaUrls === undefined || mediaTypes === undefined)
-		images = <p> "No media in post" </p>;
-	else if (tweetData.status_id === "0") {
-		images = <p> invalid ID. please refresh. </p>;
-	} else {
-		images = mediaUrls.map((url, index) => {
-			if (mediaTypes[index] === "image") {
-				return (
-					<img
-						key={url}
-						className="post"
-						src={url || undefined}
-						alt="No post retrieved. Either Twitter's CDN didn't work, the artist limited post visibility, or there is no media. Check the post."
-					></img>
-				);
-			} else {
-				return (
-					<video
-						key={url}
-						className="post"
-						controls
-						autoPlay
-						loop
-						muted
-					>
-						<source
-							src={url || undefined}
-							type="video/mp4"
-						></source>
-						Your browser does not support the video tag.
-					</video>
-				);
-			}
-		});
-	}
-
 	return (
 		<postContext.Provider value={tweetData}>
 			<refreshContext.Provider value={useRefresh}>
 				<div className="home">
-					<div className="posts" onClick={useRefresh}>{images}</div>
-					<Menu />
+					<Post/>
+					<Menu/>
 				</div>
 			</refreshContext.Provider>
 		</postContext.Provider>
---
+++ client/src/App/Post.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { refreshContext , postContext } from './Home.tsx';
+
+export const Post = () => {
+    const tweetData = React.useContext(postContext);
+    const useRefresh = React.useContext(refreshContext)
+
+    const mediaUrls = tweetData.media_urls?.split(",");
+	const mediaTypes = tweetData.media_details?.map((media) => media.type);
+
+	console.log(mediaUrls);
+	console.log(mediaTypes);
+
+	let images;
+	if (mediaUrls === undefined || mediaTypes === undefined)
+		images = <p> "No media in post" </p>;
+	else if (tweetData.status_id === "0") {
+		images = <p> invalid ID. please refresh. </p>;
+	} else {
+		images = mediaUrls.map((url, index) => {
+			if (mediaTypes[index] === "image") {
+				return (
+					<img
+						key={url}
+						className="post"
+						src={url || undefined}
+						alt="No post retrieved. Either Twitter's CDN didn't work, the artist limited post visibility, or there is no media. Check the post."
+					></img>
+				);
+			} else {
+				return (
+					<video
+						key={url}
+						className="post"
+						controls
+						autoPlay
+						loop
+						muted
+					>
+						<source
+							src={url || undefined}
+							type="video/mp4"
+						></source>
+						Your browser does not support the video tag.
+					</video>
+				);
+			}
+		});
+	}
+
+    return <div className="posts" onClick={useRefresh}>{images}</div>
+}
